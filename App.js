5e78f2771b69cbf9e2507cef706029fde8e4e3b0
quillchain <hoaanhpoker@gmail.com>
2022-03-12 11:44:50 +0700

    hello native

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const IQTESTFREE = 'https://quillchain.github.io/testiq/';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ with: '100%', hight: '100%'}}>
+        <WebView
+          source={{
+            uri: IQTESTFREE
+          }}
+          onload={console.log('loaded')}
+        />
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +24,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 28,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
